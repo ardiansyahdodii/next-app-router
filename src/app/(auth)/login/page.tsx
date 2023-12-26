1ca2faca718f1d7bd7ac970c522116b985c4099c
ardiansyahdodii <dodyn890@gmail.com>
2023-12-26 10:04:41 +0700

    group router

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,9 @@
+const LoginPage = () => {
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            Login Page
+        </main>
+    )
+}
+
+export default LoginPage
